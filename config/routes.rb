b4d4de62d57afff7eb6150cbed0c3fc6f77167f9
DGoshey <dgoshey@hotmail.com>
2010-07-27 17:15:26 -0400

    chptr5 finished adding user

--- config/routes.rb
+++ config/routes.rb
@@ -47,4 +47,6 @@ ActionController::Routing::Routes.draw do |map|
   map.about '/about', :controller => 'pages', :action => 'about'
   map.help '/help', :controller => 'pages', :action => 'help'
 
+  map.signup '/signup', :controller => 'users', :action => 'new'
+
 end
